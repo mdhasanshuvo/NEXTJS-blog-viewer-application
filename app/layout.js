6c78c11b3f1fada732086b730bb492576a4d2de3
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-01-23 09:30:58 +0600

    Project setup with next 14.2.23 version

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <nav>
+          
+        </nav>
         {children}
       </body>
     </html>
